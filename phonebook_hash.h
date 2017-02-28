40d1f3c15cfce30b7869896ee1b43c14f7b91503
Weinux <weiiawia@gmail>
2017-03-01 02:28:38 +0800

    Implement hash function
    implement hash function to phoebook, compare the effect of
    different data structures on performance

---
+++ phonebook_hash.h
@@ -0,0 +1,35 @@
+#ifndef _PHONEBOOK_H
+#define _PHONEBOOK_H
+#define HASH 1
+#define MAX_LAST_NAME_SIZE 16
+#define SIZE 9973
+
+typedef struct __PHONE_BOOK_DETAIL {
+    char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
+} detail;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    detail *pDetail;
+    struct __PHONE_BOOK_ENTRY *pNext;
+} entry;
+
+typedef struct __HASH_TABLE {
+    entry **list ;
+} hashTable;
+
+
+entry *findName(char lastname[], hashTable *ht);
+void append(char lastName[], hashTable *ht);
+void freeHashList(hashTable * ht);
+hashTable *hashTableInitial(void);
+unsigned int BKDRHash(char *str);
+#endif
